a3538affbac7f984eb75f0a4d6080e92b6f146ca
CrisRonda <cristian.j005@gmail.com>
2021-04-13 09:00:38 -0500

    fix: logger in handle error in api/

--- pages/api/utils/index.js
+++ pages/api/utils/index.js
@@ -1,7 +1,11 @@
 function handleError(res, err) {
+  console.log('*************************');
+  console.log(err);
+  console.log('*************************');
   res.status(500).send({
     status: 500,
     message: `${err.code} - ${err.message}`,
   });
+  res.end();
 }
 export default handleError;
